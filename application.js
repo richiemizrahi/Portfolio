256928b136ec75e8083ecfd11615828cca3e5257
Richie Mizrahi <richiemizrahi@gmail.com>
2013-08-22 17:02:36 -0400

    fixed rotate for all browsers

--- application.js
+++ application.js
@@ -5,5 +5,36 @@ $(document).ready(function()
            target:'body',
            offset: -55
         });
- 
+
+        $('.left-tilt img').on('mouseover', function(){ 
+        	 $(this).rotate({
+		            angle: 0,
+		            duration:50,
+		            animateTo:-7
+	          });
+		});
+
+		$('.left-tilt img').on('mouseout', function() {
+		    $(this).rotate({
+		            angle: -7,
+		            duration:50,
+		         	animateTo:0
+			});
+		});
+
+		$('.right-tilt img').on('mouseover', function(){ 
+        	 $(this).rotate({
+		            angle: 0, 
+		            duration:50,
+		            animateTo:7
+	          });
+		});
+
+		$('.right-tilt img').on('mouseout', function() {
+		    $(this).rotate({
+		            angle: 7,
+		            duration:50,
+		         	animateTo:0
+			});
+		}); 
     });
